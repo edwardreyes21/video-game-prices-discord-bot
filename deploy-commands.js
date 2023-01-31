bc439f233a53a8e2c9a46daf8fa5bc754ba18f4a
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-01-30 22:22:18 -0800

    Created deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { REST, Routes, RESTJSONErrorCodes } = require('discord.js');
+const fs = require('node:fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`.commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10 '}).setToken(process.env.DISCORD_BOT_TOKEN);
