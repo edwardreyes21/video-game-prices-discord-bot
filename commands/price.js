1ac9af6f08064aff91c4e4038ce1a6d3beb38a3d
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-02-02 21:49:48 -0800

    Added function to return message to user

--- commands/price.js
+++ commands/price.js
@@ -3,5 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('price')
-    .setDescription('Usage: /price {gameName} {region}')
+    .setDescription('Usage: /price {gameName} {region}'),
+    async execute(interaction) {
+        await interaction.reply(`/price is called`);
+    }
 };
