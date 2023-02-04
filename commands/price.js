2bbcd7a57a6801eadb76fb67774386152c861a5b
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-02-03 22:40:28 -0800

    Added game name option for /price command

--- commands/price.js
+++ commands/price.js
@@ -1,10 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('price')
-    .setDescription('Usage: /price {gameName} {region}'),
+    .setDescription('Usage: /price {gameName} {region}')
+    .addStringOption(option =>
+        option.setName('gamename')
+            .setDescription('The name of the video game')
+            .setRequired(true)),
     async execute(interaction) {
+        let gameName = interaction.options.get('gamename');
+        console.log(gameName.value);
+
         await interaction.reply(`/price is called`);
     }
 };
