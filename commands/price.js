9bbe925ac60841a95a0ab1d385920cc6e1cd05ee
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-02-09 00:45:53 -0800

    New rich embedded message for video game prices

--- commands/price.js
+++ commands/price.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 
 module.exports = {
@@ -13,12 +13,23 @@ module.exports = {
     async execute(interaction) {
         let gameName = interaction.options.get('gamename');
 
-        interaction.reply(`/price is called, gamename: ${gameName.value}`);
+        // interaction.reply(`/price is called, gamename: ${gameName.value}`);
 
         try {
             let response = await axios.get(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ISTHEREANYDEALS_TOKEN}&plains=${gameName.value}`);
             let price = response.data;
-            console.log(price);
+            let gameData = price.data;
+
+            // console.log(price);
+            // console.log(gameData);
+            // console.log(gameData[gameName.value].list);
+
+            const pricesMessage = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`${gameName.value} Current Prices`);
+
+            interaction.reply({ embeds: [pricesMessage] });
+
         }
         catch (error) {
             console.log(error);
