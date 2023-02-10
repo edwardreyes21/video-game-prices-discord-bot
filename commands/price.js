1da47d75d1c78bdfc8f32760ef07a902d614af24
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-02-09 22:09:28 -0800

    gamename converts to lowercase and no whitespace

--- commands/price.js
+++ commands/price.js
@@ -11,23 +11,26 @@ module.exports = {
             .setDescription('The name of the video game')
             .setRequired(true)),
     async execute(interaction) {
-        let gameName = interaction.options.get('gamename');
+        let gameObj = interaction.options.get('gamename');
+        
+        let gameName = gameObj.value;
+        gameName = gameName.replace(/\s/g, '').toLowerCase();
 
         try {
-            let response = await axios.get(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ISTHEREANYDEALS_TOKEN}&plains=${gameName.value}`);
+            let response = await axios.get(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ISTHEREANYDEALS_TOKEN}&plains=${gameName}`);
             let price = response.data;
             let gameData = price.data;
 
             console.log(price);
             console.log(gameData);
-            console.log(gameData[gameName.value].urls.game);
+            console.log(gameData[gameName].urls.game);
 
-            let priceList = gameData[gameName.value].list;
+            let priceList = gameData[gameName].list;
 
             const pricesMessage = new EmbedBuilder()
                 .setColor(0x0099FF)
-                .setTitle(`${gameName.value} Current Prices`)
-                .setURL(gameData[gameName.value].urls.game);
+                .setTitle(`${gameName} Current Prices`)
+                .setURL(gameData[gameName].urls.game);
 
             for (const priceobj of priceList) {
                 pricesMessage.addFields(
