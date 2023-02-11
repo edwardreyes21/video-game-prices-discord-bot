94b4521a6b21e0d9ae8e62f592991e32b61698c8
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-02-10 23:55:33 -0800

    Uses 'get plain' API call to identify game name

--- commands/price.js
+++ commands/price.js
@@ -13,37 +13,62 @@ module.exports = {
     async execute(interaction) {
         let gameObj = interaction.options.get('gamename');
 
-        let gameName = gameObj.value;
-        gameName = gameName.replace(/\s/g, '').toLowerCase();
+        let userInput = gameObj.value;
+        userInput = userInput.replace(/\s/g, '').toLowerCase();
 
+        let plainName = "", giveError = false;
         try {
-            let response = await axios.get(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ISTHEREANYDEALS_TOKEN}&plains=${gameName}`);
-            let price = response.data;
-            let gameData = price.data;
-
-            console.log(price);
-            console.log(gameData);
-            console.log(gameData[gameName].urls.game);
-
-            let priceList = gameData[gameName].list;
-            console.log(priceList);
-
-            const pricesMessage = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle(`${gameName} Current Prices`)
-                .setURL(gameData[gameName].urls.game);
-
-            for (const priceobj of priceList) {
-                pricesMessage.addFields(
-                    { name: `${priceobj.price_new} at ${priceobj.shop.name}`, value: `${priceobj.url}` }
-                );
+            let plainObj = await axios.get(`https://api.isthereanydeal.com/v02/game/plain/?key=${process.env.ISTHEREANYDEALS_TOKEN}&title=${userInput}`);
+            if ('plain' in plainObj.data.data) {
+                plainName = plainObj.data.data.plain;
             }
-
-            interaction.reply({ embeds: [pricesMessage] });
-
+            else {
+                giveError = true;
+            }
         }
         catch (error) {
             console.log(error);
+        }
+
+        if (giveError || plainName.length === 0) {
+            const errorMessage = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`Failed to Retrieve Game`)
+                .addFields( 
+                    { name: `Sorry, but we were not able to find the game you were looking for.`, value: `Perhaps try a more specific title?` }
+                );
+
+            interaction.reply({ embeds: [errorMessage] });
+        } else {
+            try {
+                let response = await axios.get(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ISTHEREANYDEALS_TOKEN}&plains=${plainName}`);
+                let price = response.data;
+                let gameData = price.data;
+    
+                console.log(price);
+                console.log(gameData);
+                console.log(gameData[plainName].urls.game);
+    
+                let priceList = gameData[plainName].list;
+                console.log(priceList);
+    
+                const pricesMessage = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(`${plainName} Current Prices`)
+                    .setURL(gameData[plainName].urls.game);
+    
+                for (const priceobj of priceList) {
+                    pricesMessage.addFields(
+                        { name: `${priceobj.price_new} at ${priceobj.shop.name}`, value: `${priceobj.url}` }
+                    );
+                }
+    
+                interaction.reply({ embeds: [pricesMessage] });
+    
+            }
+            catch (error) {
+                console.log(error);
+            }
         }
     }
 };
